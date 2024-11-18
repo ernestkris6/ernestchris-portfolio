530b8b41f4f1bb3e2709406cb39142ede30b0a0f
ERNEST CHRIS <ernestchris6@gmail.com>
2024-11-19 00:06:43 +0100

    progress

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -8,7 +8,7 @@ export default function AboutMe() {
   return (
     <div className='bg-[#121212] text-white mt-[-2px] pt-8'>
         <h1 className='text-center font-medium text-3xl md:text-5xl'>About Me</h1>
-        <img src={theme} alt="" />
+        <img className='z-[-1] left-0 bottom-0 top-0 right-0' src={theme} alt="" />
 
        <div className='flex justify-center pt-6 gap-4 pb-4'>
             <img className='hidden md:flex rounded-lg' src={profile} alt="" />
@@ -19,27 +19,28 @@ export default function AboutMe() {
                 fuga modi. Explicabo 
                 cum enim ipsam tenetur 
                 distinctio debitis saepe incidunt?</p> 
-                <div className=''>
-                  <p>HTML & CSS <hr className='w-[90%]' /></p>
-                  
+
+                <div className='flex pt-12 gap-[20px] items-center transition-all'>
+                  <p className='min-w-[150px] text-medium font-bold'>HTML & CSS </p>
+                  <hr className='w-[90%] outline-none border-none h-8 rounded-lg h-0.5 bg-gradient-to-r from-red-500 via-orange-500 to-purple-500' />
                 </div>
-                <div>
-                  <p>React JS <hr className='w-[85%]'/></p>
-                  
+                <div className='flex gap-[20px] items-center transition-all'>
+                  <p className='min-w-[150px] text-medium font-bold'>React JS</p>
+                  <hr className='w-[80%] outline-none border-none h-8 rounded-lg h-0.5 bg-gradient-to-r from-red-500 via-orange-500 to-purple-500'/>
                 </div>
 
-                <div>
-                  <p>Javascript</p>
-                  <hr className='w-[80%]'/>
+                <div className='flex gap-[20px] items-center transition-all'>
+                  <p className='min-w-[150px] text-medium font-bold'>Javascript</p>
+                  <hr className='w-[70%] outline-none border-none h-8 rounded-lg h-0.5 bg-gradient-to-r from-red-500 via-orange-500 to-purple-500'/>
                 </div>
 
-                <div>
-                  <p>Next JS</p>
-                  <hr className='w-[60%]'/>
+                <div className='flex gap-[20px] items-center transition-all'>
+                  <p className='min-w-[150px] text-medium font-bold'>Next JS</p>
+                  <hr className='w-[60%] outline-none border-none h-8 rounded-lg h-0.5 bg-gradient-to-r from-red-500 via-orange-500 to-purple-500'/>
                 </div>
-                <div>
-                  <p>Wordpress</p>
-                  <hr className='w-[84%]'/>
+                <div className='flex gap-[20px] items-center transition-all'>
+                  <p className='min-w-[150px] text-medium font-bold'>Wordpress</p>
+                  <hr className='w-[84%] outline-none border-none h-8 rounded-lg h-0.5 bg-gradient-to-r from-red-500 via-orange-500 to-purple-500'/>
                 </div>          
 
             </div>
