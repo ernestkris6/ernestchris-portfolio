f814146ba8b61eec3e656c163261c098d8515a15
ERNEST CHRIS <ernestchris6@gmail.com>
2024-11-21 09:16:30 +0100

    adding images

--- src/Components/MyLatestWork.jsx
+++ src/Components/MyLatestWork.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import servicesData from '../data'
+// import servicesData from '../data'
 import Button from '../Common/Button'
+import mywork_data from '../works'
 
 export default function MyLatestWork() {
   return (
@@ -9,12 +10,12 @@ export default function MyLatestWork() {
             <h1 className='font-semibold text-white text-3xl md:text-5xl'>My latest work</h1> 
         </div>  
         <div className='flex flex-col md:grid md:grid-cols-[1fr_1fr] lg:grid-cols-[1fr_1fr_1fr] mx-6 gap-4 pt-5 sm:px-0'>
-            {servicesData.map((item, index)=> (
+            {mywork_data.map((item, index)=> (
                 <div className='border border-blue-200 rounded-md overflow-hidden px-8 py-10 cursor-pointer hover:translate-y-[-10px] transition-all duration-500 text-white hover:bg-gradient-to-b from-red-500 via-orange-500 to-purple-500' key={index}>
-                    <p className='font-bold text-xl'>0{item.id}</p>
-                    <h1 className='font-bold text-xl'>{item.header}</h1>
-                    <p className='mt-4'>{item.body}</p>
-                    <p className='mt-4 py-2 rounded-full md:w-1/2'>{item.btn}</p>
+                    <p className='font-bold text-xl'>0{item.no}</p>
+                    <img className='font-bold text-xl' src={item.img} alt=''/>
+                    {/* <p className='mt-4'>{item.body}</p>
+                    <p className='mt-4 py-2 rounded-full md:w-1/2'>{item.btn}</p> */}
                 </div>
             ))}
         </div>
---
+++ src/works.js
@@ -0,0 +1,42 @@
+import project1_img from './assets/project_1.svg'
+import project2_img from './assets/project_2.svg'
+import project3_img from './assets/project_3.svg'
+import project4_img from './assets/project_4.svg'
+import project5_img from './assets/project_5.svg'
+import project6_img from './assets/project_6.svg'
+
+
+const mywork_data = [
+    {
+    no:1,
+    name:"Web design",
+    img:project1_img
+    },
+    {
+    no:2,
+    name:"Web design",
+    img:project2_img
+    },
+    {
+    no:3,
+    name:"Web design",
+    img:project3_img
+    },
+    {
+    no:4,
+    name:"Web design",
+    img:project4_img
+    },
+    {
+    no:5,
+    name:"Web design",
+    img:project5_img
+    },
+    {
+    no:6,
+    name:"Web design",
+    img:project6_img
+    },
+]
+ 
+export default mywork_data;
