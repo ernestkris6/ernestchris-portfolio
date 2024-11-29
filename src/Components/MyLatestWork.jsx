841c600684e298613cc2db7596f1d4559f7c0b6d
ERNEST CHRIS <ernestchris6@gmail.com>
2024-11-29 23:00:12 +0100

    latest works

--- src/Components/MyLatestWork.jsx
+++ src/Components/MyLatestWork.jsx
@@ -7,11 +7,11 @@ export default function MyLatestWork() {
   return (
     <div id='works' className='mx-[1px] md:mx-[10%]'>     
         <div className='text-center py-8'>
-            <h1 className='font-semibold text-white text-3xl md:text-5xl'>My latest work</h1> 
+            <h1 className='font-semibold pt-12 text-white text-3xl md:text-5xl'>My latest work</h1> 
         </div>  
         <div className='flex flex-col md:grid md:grid-cols-[1fr_1fr] lg:grid-cols-[1fr_1fr_1fr] mx-6 gap-4 pt-5 sm:px-0'>
             {mywork_data.map((item, index)=> (
-                <div className='border border-blue-200 rounded-md overflow-hidden px-8 py-10 cursor-pointer hover:translate-y-[-10px] transition-all duration-500 text-white hover:bg-gradient-to-b from-red-500 via-orange-500 to-purple-500' key={index}>
+                <div className='overflow-hidden px-8 py-10 cursor-pointer hover:translate-y-[-10px] transition-all duration-500 text-white' key={index}>
                     <p className='font-bold text-xl'>0{item.no}</p>
                     <img className='font-bold text-xl' src={item.img} alt=''/>
                     {/* <p className='mt-4'>{item.body}</p>
