bb383c0163c0af4ff995c2c76950399229410b04
ERNEST CHRIS <ernestchris6@gmail.com>
2024-12-04 22:05:56 +0100

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,20 +4,21 @@ import Button from '../Common/Button';
 
 export default function Footer() {
   return (
-    <div className='mt-[-4px]'>
+    <div className='mx-[1px] md:mx-[10%]'>
       <div className='flex justify-between'>
         <div className='flex flex-col mb-6'>
-        <img className='h-12 w-32 mt-2 ml-6 mb-2' src={logo} alt="" />
+        <img className='h-12 w-24 md:w-32 mt-2 ml-6 mb-2' src={logo} alt="" />
          <p className='mx-6 text-white'>Lorem ipsum dolor, sit <br/> amet consectetur adipisicing elit. Quam esse vitae rem,</p>
 
-        </div>
-     
-        <div className='mx-6'>
+         <div className='p-8'>
           <form>
-            <input className='bg-[#ffffff1a] py-2 px-4 rounded-full' type="text" placeholder='Enter your name'/>
+            <input className='bg-[#ffffff1a] text-white py-2 px-2 rounded-full' type="text" placeholder='Enter your name'/>
             <Button className='mt-4 ml-2 bg-gradient-to-r from-red-500 via-orange-500 to-purple-500 px-8 py-2 rounded-full mb-8 text-white font-medium hover:transition-all duration-500 hover:scale-105'>Subscribe</Button>
           </form>
         </div>
+        </div>
+     
+        
       
       </div>
         <hr className='h-4 mt-[-16px] mx-6' />
