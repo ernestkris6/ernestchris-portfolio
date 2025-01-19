df7129694a28308aa57e42ab4a10c3bf2f112671
ERNEST CHRIS <ernestchris6@gmail.com>
2025-01-19 12:31:01 +0100

    connect with me btn

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -5,7 +5,9 @@ export default function Buttons() {
   return (
     <div className='pb-8'>
         <div className='grid md:flex gap-6 justify-center py-2'>
+          <a href="https://web.facebook.com/ernest.chris.503">
             <Button className='flex items-center bg-gradient-to-r from-red-500 via-orange-500 to-purple-500 py-3 px-12 rounded-full text-white  cursor-pointer hover:bg-blue-500 hover:scale-105 transition-all duration-500'>Connect with me</Button>
+          </a>
             
             <a href="https://web.facebook.com/?_rdc=1&_rdr#"
                target='_blank'
