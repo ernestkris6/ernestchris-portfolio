a3498a3091348df3a68b17991dcccc9495e25862
ERNEST CHRIS <ernestchris6@gmail.com>
2025-03-27 18:31:58 +0100

    adding my photos

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import profile from '../assets/profile_pic.png';
+// import profile from '../assets/profile_pic.png';
+import krisnew3 from '../assets/krisnew3.png';
 import theme from '../assets/theme.svg'
 
 
@@ -13,7 +14,7 @@ export default function AboutMe() {
         </div>
         {/* z-[-1] left-0 bottom-0 top-0 right-0 */}
        <div className='flex justify-center pt-6 gap-4 pb-4'>
-            <img className='hidden md:flex rounded-lg' src={profile} alt="" />
+            <img className='hidden md:flex rounded-lg' src={krisnew3} alt="" />
             <div className='w-full md:w-2/4 ml-2 mr-2'>
                <p className='text-white md:text-medium lg:text-xl'>I develop responsive websites with quality user interface. With codes I bring your imaginations to life. If you can picture it I can capture it!!!</p> 
 
--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import profile from '../assets/profile_pic.png'
+// import profile from '../assets/profile_pic.png'
+// import krispix2 from '../assets/chrispix2.jpg'
 // import Button from '../Common/Button'
+// import chrispix1 from '../assets/chrispix1.jpg'
+import chrispix3 from '../assets/chrispix3.jpg'
 
 export default function HeroSection() {
   return (
     <section id='/' className='hero'>
         <div className='flex m-auto justify-center items-center py-8'>
-            <img className='rounded-full' src={profile} alt="" />
+            <img className='rounded-xl w-72' src={chrispix3} alt="" />
         </div>
            <p className='text-white text-2xl font-bold md:text-3xl text-center'><span>I'm Ernest Christopher,</span> <br /> frontend developer based in Lagos, Nigeria.</p>
             <div className='text-white w-3/4 text-center md:w-2/4 mx-auto py-6'>
